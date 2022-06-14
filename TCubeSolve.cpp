29f3752cc902898e4925c3dc609c4fdaf0a6e392
William Netz-Mowrer <wnetzmowrer@gmail.com>
2022-06-13 19:21:30 -0700

    ahhhhhuuuuug

--- TCubeSolve.cpp
+++ TCubeSolve.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <unistd.h>
 using namespace std;
 
 const int orientations [12][3][3] =
@@ -11,7 +16,7 @@ const int orientations [12][3][3] =
     //          z1                        z2                        z3                         z4
     {{0,0,1},{0,0,2},{1,0,1}},{{0,0,1},{0,0,2},{0,1,1}},{{0,0,1},{0,0,2},{-1,0,1}},{{0,0,1},{0,0,2},{0,-1,1}}
 };
-const int x1 [3][3] = {{1,0,0},{2,0,0},{1,1,0}};
+/*const int x1 [3][3] = {{1,0,0},{2,0,0},{1,1,0}};
 const int x2 [3][3] = {{1,0,0},{2,0,0},{1,0,1}};
 const int x3 [3][3] = {{1,0,0},{2,0,0},{1,-1,0}};
 const int x4 [3][3] = {{1,0,0},{2,0,0},{1,0,-1}};
@@ -22,40 +27,55 @@ const int y4 [3][3] = {{0,1,0},{0,2,0},{0,1,-1}};
 const int z1 [3][3] = {{0,0,1},{0,0,2},{1,0,1}};
 const int z2 [3][3] = {{0,0,1},{0,0,2},{0,1,1}};
 const int z3 [3][3] = {{0,0,1},{0,0,2},{-1,0,1}};
-const int z4 [3][3] = {{0,0,1},{0,0,2},{0,-1,1}};
+const int z4 [3][3] = {{0,0,1},{0,0,2},{0,-1,1}};*/
+ofstream outFile;
 
-bool inRange(const int orientation[3][3], vector<int> point)
+long getMemusage()
+{
+    struct rusage myusage;
+    getrusage(RUSAGE_SELF, &myusage);
+    return myusage.ru_maxrss;
+}
+bool isOpen(int (* cube)[6][6][6],const int orientation[3][3], vector<int> point)
 {
     bool in = true;
+    bool open = true;
     for(int i = 0; i < 3; i++)
     {
         for(int p = 0; p < 3; p++)
         {
-            if(orientation[i][p] + point[p] < 0 && orientation[i][p] + point[p] > 5)
+            if(orientation[i][p] + point[p] < 0 || orientation[i][p] + point[p] > 5)
             {
                 in = false;
+            }            
+        }
+        if(in)
+        {
+            if((*cube)[orientation[i][0] + point[0]][orientation[i][1] + point[1]][orientation[i][2] + point[2]] != -1)
+            {
+                outFile << "colision at {" << orientation[i][0] + point[0] << ", " << orientation[i][1] + point[1] << ", " << orientation[i][2] + point[2] << "}"<<endl;
+                open = false;
             }
         }
     }
-    return in;
+    return open && in;
 }
-
 int addT(int (* cube)[6][6][6], vector<int> point)
 {
-    cout <<"addT "<< point[0] << " " << point[1] << " " << point[2] << endl;
+    outFile <<"addT "<< point[0] << " " << point[1] << " " << point[2] << endl;
     int orientation = -2;
     for(int i = 0; i < 12; i++)
     {
-        cout<< "orientation " << i << endl;
-        if(inRange(orientations[i],point))
+        outFile<< "orientation " << i << endl;
+        if(isOpen(cube,orientations[i],point))
         {    
-            cout << "in" << endl;                   
+            outFile << "in" << endl;                   
             (*cube)[point[0]][point[1]][point[2]] = i;
-            printf("{%d,%d,%d} = %d\n",point[0],point[1],point[2],i);
+            outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"} = "<<i<<endl;
             for(int j = 0; j < 3; j++)
             {
                 (*cube)[point[0] + orientations[i][j][0]][point[1] + orientations[i][j][1]][point[2] + orientations[i][j][2]] = i;
-                printf("{%d,%d,%d} = %d\n",point[0] + orientations[i][j][0],point[1] + orientations[i][j][1],point[2] + orientations[i][j][2],i);
+                outFile <<"{"<<point[0] + orientations[i][j][0]<<","<<point[1] + orientations[i][j][1]<<","<<point[2] + orientations[i][j][2]<<"} = "<<i<<endl;
             }
             orientation = i;
             i = 12;
@@ -63,7 +83,6 @@ int addT(int (* cube)[6][6][6], vector<int> point)
     }
     return orientation;
 }
-
 void updateExplored (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cubeExplored, vector<vector<int>> newExplored)
 {
     //remove newExplored points from frontier
@@ -93,18 +112,18 @@ void updateExplored (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cub
         }
         index ++;
     }
-    cout << "frontier -  newExplored:"; 
+    outFile << "frontier -  newExplored:"; 
     for(vector<int> point :(*cubeFrontier))
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
     
     //add newExplored points to explored
     index = 0;
     if(cubeExplored->size() == 0)
     {
-        cout << "0 cubeExplored" << endl;
+        outFile << "0 cubeExplored" << endl;
         cubeExplored->insert(cubeExplored->end(),newExplored.begin(),newExplored.end());
     }
     else
@@ -135,14 +154,13 @@ void updateExplored (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cub
             index ++;
         }
     }
-    cout << "explored + newExplored:"; 
+    outFile << "explored + newExplored:"; 
     for(vector<int> point :(*cubeExplored))
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
 }
-
 void updateFrontier (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cubeExplored, vector<vector<int>> newExplored)
 {
     //get all possible new frontier points in range 
@@ -174,12 +192,12 @@ void updateFrontier (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cub
             newFrontier.push_back(vector<int>{point[0],point[1],point[2] - 1});
         }
     }
-    cout << "newFrontier:"; 
+    outFile << "newFrontier:"; 
     for(vector<int> point :newFrontier)
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
     
     //remove explored points from newFrontier
     int index = 0;
@@ -208,18 +226,18 @@ void updateFrontier (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cub
         }
         index ++;
     }
-    cout << "newFrontier - explored:"; 
+    outFile << "newFrontier - explored:"; 
     for(vector<int> point :newFrontier)
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
     
     //add newFrontier points to frontier
     index = 0;
     if(cubeFrontier->size() == 0)
     {
-        cout << "0 cubeFrontier" << endl;
+        outFile << "0 cubeFrontier" << endl;
         for(int i = 0; i < newFrontier.size(); i++)
         {
             bool in = false;
@@ -272,14 +290,13 @@ void updateFrontier (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cub
             index ++;
         }
     }
-    cout << "frontier + newFrontier:"; 
+    outFile << "frontier + newFrontier:"; 
     for(vector<int> point :(*cubeFrontier))
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
 }
-
 void update (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cubeExplored, int orientation)
 {
     //get all new explored points
@@ -288,17 +305,15 @@ void update (vector<vector<int>> *cubeFrontier ,vector<vector<int>> *cubeExplore
     {
         newExplored.push_back(vector<int>{newExplored[0][0] + orientations[orientation][i][0],newExplored[0][1] + orientations[orientation][i][1],newExplored[0][2] + orientations[orientation][i][2]});
     }
-    cout << "newExplored :"; 
+    outFile << "newExplored :"; 
     for(vector<int> point :newExplored)
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        outFile <<"{"<<point[0]<<","<<point[1]<<","<<point[2]<<"}";
     }
-    cout<<endl;
+    outFile<<endl;
     
     updateExplored(cubeFrontier,cubeExplored,newExplored); 
-    updateFrontier(cubeFrontier,cubeExplored,newExplored); 
-    
-     
+    updateFrontier(cubeFrontier,cubeExplored,newExplored);  
 }
 
 int main()
@@ -306,8 +321,9 @@ int main()
     int cube [6][6][6];
     vector<vector<int>> cubeFrontier {vector<int> (3, 0)};
     vector<vector<int>> cubeExplored;
-    
-    cout<< "sdrghfd"<< endl;
+    outFile.open("output.txt");
+
+    outFile<< "sdrghfd"<< endl;
     for(int x = 0; x < 6; x++) 
     {
         for(int y = 0; y < 6; y++) 
@@ -318,23 +334,54 @@ int main()
             }
         }
     }
-    cout<<"Frontier : ";
-    for(vector<int> point :cubeFrontier )
+    outFile<<endl;
+    int tAdded = 0;
+    while(cubeFrontier.size() > 0)
     {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
+        int orientation = addT(&cube,cubeFrontier [0]);
+        if(orientation != -2)
+        {
+            update (&cubeFrontier,&cubeExplored,orientation);
+            outFile << tAdded << " t's" << endl;
+            for(int i = 0; i < 6; i++)
+            {
+                string layer = "";
+                bool allEmpty = true;
+                for(int j = 0; j < 6; j++)
+                {
+                    for(int k = 0; k < 6; k++)
+                    {
+                        if(cube[k][i][j] != -1)
+                        {
+                            allEmpty = false;
+                            if(cube[k][i][j] < 10)
+                            {
+                                layer = layer + "0" + to_string(cube[k][i][j]) + ", ";
+                            }
+                            else
+                            {
+                                layer = layer + to_string(cube[k][i][j]) + ", ";
+                            }
+                        }
+                        else
+                        {
+                            layer = layer + to_string(cube[k][i][j]) + ", ";
+                        }                        
+                    }
+                    layer = layer + "\n";
+                }
+                if(!allEmpty)
+                {
+                    outFile << layer << endl;
+                }
+            }
+            tAdded++;
+        }
+        else
+        {
+            outFile << "erase {" << cubeFrontier[0][0] << ", " << cubeFrontier[0][1] << ", " << cubeFrontier[0][2] << "}" << endl;
+            cubeFrontier.erase(cubeFrontier.begin());
+        }
     }
-    cout<<endl;
-    cout<<"Explored: ";
-    for(vector<int> point :cubeExplored)
-    {
-        printf("{%d,%d,%d}",point[0],point[1],point[2]);
-    }
-    cout<<endl;
-    int a = 3;
-    while(a > 0)
-    {
-        int orientation = addT(&cube, cubeFrontier [0]);
-        update (&cubeFrontier ,&cubeExplored,orientation);
-        a--;
-   }
+    outFile.close();
 }
